6d6dbcb1fd6a6317d89f7bf4d4d6bde182fa78a0
Chintan Shah <chintan@wohlig.com>
2016-11-28 16:32:40 +0530

    https

--- config/env/production.js
+++ config/env/production.js
@@ -26,7 +26,7 @@ module.exports = {
    ***************************************************************************/
 
   port: 81,
-  realHost: "http://rusa.wohlig.co.in",
+  realHost: "https://rusa.wohlig.co.in",
   emails: ["chintan@wohlig.com", "jagruti@wohlig.com", "tushar@wohlig.com", "chirag@wohlig.com", "harsh@wohlig.com"]
 
   /***************************************************************************
