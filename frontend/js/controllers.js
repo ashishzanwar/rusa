4d6fb391a6813096094a31a485187910c3bebcbc
sohan1 <shn619@gmail.com>
2017-02-11 17:00:17 +0530

    controller model

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -33,6 +33,106 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
         $scope.menutitle = NavigationService.makeactive("Institute Form"); //This is the Title of the Website
         TemplateService.title = $scope.menutitle;
         $scope.navigation = NavigationService.getnav();
+        var institutetoVendors = {
+            vendorName: "Vendor1",
+            installmentNo: "installmentNo",
+            amount: "amount",
+            transactionRececived: "transactionRececived",
+            remarks: "remarks",
+            file: "file"
+        }
+        var projectExpenses = {
+            name: "Project Expense 1",
+            installmentNo: "installmentNo",
+            amount: "amount",
+            transactionSent: "transactionSent",
+            transactionRececived: "transactionRececived",
+            remarks: "remarks",
+            file: "file",
+            institutetoVendors: [institutetoVendors]
+        }
+
+        var project = {
+            name: "Project1",
+            projectType: "projectType",
+            assetType: "assetType",
+            photo1: "photo1",
+            photo2: "photo2",
+            valueProject: "valueProject",
+            dueDate: "dueDate",
+            projectExpenses: [projectExpenses],
+        }
+
+
+        $scope.projects = [project];
+
+        $scope.addNewProject = function (index) {
+            $scope.projects.push(project);
+        };
+        $scope.removeProject = function (index) {
+            $scope.projects.splice(index, 1);
+            console.log(index, 'deleted');
+        };
+
+        $scope.addNewProjectExpense = function (arr) {
+            arr.push(projectExpenses);
+        };
+        $scope.removeProjectExpense = function (arry, index) {
+            console.log(arry, 'deleted');
+            arry.splice(index, 1);
+            console.log(arry, 'deleted');
+        };
+
+
+        $scope.addNewinstitutetoVendors = function (arr1) {
+            arr1.push(institutetoVendors);
+        };
+        $scope.removeinstitutetoVendors = function (arry, index) {
+            console.log(arry, 'deleted');
+            arry.splice(index, 1);
+            console.log(arry, 'deleted');
+        };
+
+
+
+
+        var state = {
+            name: "Center to State 1",
+            installmentNo: "installmentNo",
+            amount: "amount",
+            transactionSent: "transactionSent",
+            transactionRececived: "transactionRececived",
+            remarks: "remarks",
+            file: "file"
+        };
+        $scope.states = [state]
+        $scope.addNewState = function (index) {
+            $scope.states.push(state);
+        };
+
+        $scope.removeState = function (index) {
+            $scope.states.splice(index, 1);
+        };
+
+        var institute = {
+            name: "State to Institute 1",
+            installmentNo: "",
+            amount: "",
+            transactionSent: "",
+            transactionRececived: "",
+            remarks: "",
+            file: ""
+
+        }
+        $scope.institutes = [institute];
+
+        $scope.addNewInstitute = function (index) {
+            $scope.institutes.push(institute);
+        };
+
+        $scope.removeInstitute = function (index) {
+            $scope.institutes.splice(index, 1);
+        };
 
     })
 
