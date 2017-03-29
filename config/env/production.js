958a1b49e8d2e90edd4730c9f764c5e3751a1981
Chintan <chintan@wohlig.com>
2017-03-29 15:22:04 +0530

    done

--- config/env/production.js
+++ config/env/production.js
@@ -27,7 +27,7 @@ module.exports = {
 
   port: 85,
   // realHost: "https://rusabeta.wohlig.co.in",
-  realHost: "http://rusabeta.wohlig.com",
+  realHost: "https://rusa.thegraylab.com",
   emails: ["chintan@wohlig.com", "ashish.zanwar@wohlig.com", "jagruti@wohlig.com", "tushar@wohlig.com", "chirag@wohlig.com", "harsh@wohlig.com", "sohan@wohlig.com", "pratik.gawand@wohlig.com"]
 
 
